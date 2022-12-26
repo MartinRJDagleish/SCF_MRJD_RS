0837e6aa926e7669c0f934ad8a7b4a9fd69bd49c
MartinRJDagleish <martin.dagleish@stud.uni-regensburg.de>
2022-12-26 13:25:07 +0100

    Almost finished with Torsion angle

--- src/main.rs
+++ src/main.rs
@@ -62,6 +62,14 @@ impl Molecule {
         return vec_cros_prod;
     }
 
+    fn calc_scalar_prod(vec1: &Vec<f64>, vec2: &Vec<f64>) -> f64 {
+        let mut scalar_prod: f64 = 0.0;
+        for cart_coord in 0..3 {
+            scalar_prod += vec1[cart_coord] * vec2[cart_coord];
+        }
+        return scalar_prod;
+    }
+
     fn calc_bond_length(geom: &Array2<f64>, idx1: usize, idx2: usize) -> f64 {
         let mut bond_length: f64 = 0.0;
         for cart_coord in 0..3 {
@@ -87,16 +95,81 @@ impl Molecule {
             bond_angle += -unit_ij[cart_coord] * unit_jk[cart_coord];
         }
 
-        return bond_angle.acos() * (180.0 / std::f64::consts::PI);
+        return bond_angle.acos().to_degrees();
     }
 
-    fn calc_scalar_prod(vec1: &Vec<f64>, vec2: &Vec<f64>) -> f64 {
-        let mut scalar_prod: f64 = 0.0;
+    fn calc_oop_angle(&self, i: usize, j: usize, k: usize, l: usize) -> f64 {
+        let bond_dist_jk: f64 = Molecule::calc_bond_length(&self.geom, j, k);
+        let bond_dist_kl: f64 = Molecule::calc_bond_length(&self.geom, k, l);
+        let bond_dist_ik: f64 = Molecule::calc_bond_length(&self.geom, i, k);
+
+        let mut unit_kj: Vec<f64> = vec![0.0; 3];
+        let mut unit_kl: Vec<f64> = vec![0.0; 3];
+        let mut unit_ki: Vec<f64> = vec![0.0; 3];
+
         for cart_coord in 0..3 {
-            scalar_prod += vec1[cart_coord] * vec2[cart_coord];
+            unit_kj[cart_coord] =
+                (self.geom[(k, cart_coord)] - self.geom[(j, cart_coord)]) / bond_dist_jk;
+            unit_kl[cart_coord] =
+                (self.geom[(k, cart_coord)] - self.geom[(l, cart_coord)]) / bond_dist_kl;
+            unit_ki[cart_coord] =
+                (self.geom[(k, cart_coord)] - self.geom[(i, cart_coord)]) / bond_dist_ik;
         }
-        return scalar_prod;
+
+        let cross_prod: Vec<f64> = Molecule::calc_vec_cros_prod(&unit_kj, &unit_kl);
+
+        let mut oop_angle: f64 = (Molecule::calc_scalar_prod(&cross_prod, &unit_ki))
+            / Molecule::calc_bond_angle(&self.geom, j, k, l).sin();
+
+        if oop_angle < -1.0 {
+            oop_angle = -1.0f64.asin();
+        } else if oop_angle > 1.0 {
+            oop_angle = 1.0f64.asin();
+        } else {
+            oop_angle = oop_angle.asin();
+        }
+
+        return oop_angle.to_degrees();
     }
+
+    fn calc_dihedral_angle(&self, i: usize, j: usize, k: usize, l: usize) -> f64 {
+        let bond_dist_ij: f64 = Molecule::calc_bond_length(&self.geom, i, j);
+        let bond_dist_jk: f64 = Molecule::calc_bond_length(&self.geom, j, k);
+        let bond_dist_kl: f64 = Molecule::calc_bond_length(&self.geom, k, l);
+
+        let mut unit_ij: Vec<f64> = vec![0.0; 3];
+        let mut unit_jk: Vec<f64> = vec![0.0; 3];
+        let mut unit_kl: Vec<f64> = vec![0.0; 3];
+
+        for cart_coord in 0..3 {
+            unit_ij[cart_coord] =
+                (self.geom[(i, cart_coord)] - self.geom[(j, cart_coord)]) / bond_dist_ij;
+            unit_jk[cart_coord] =
+                (self.geom[(j, cart_coord)] - self.geom[(k, cart_coord)]) / bond_dist_jk;
+            unit_kl[cart_coord] =
+                (self.geom[(k, cart_coord)] - self.geom[(l, cart_coord)]) / bond_dist_kl;
+        }
+
+        let cross_prod_1: Vec<f64> = Molecule::calc_vec_cros_prod(&unit_ij, &unit_jk);
+        let cross_prod_2: Vec<f64> = Molecule::calc_vec_cros_prod(&unit_jk, &unit_kl);
+
+        let numerator: f64 = Molecule::calc_scalar_prod(&cross_prod_1, &cross_prod_2);
+        let denom: f64 = Molecule::calc_bond_angle(&self.geom, i, j, k).sin()
+            * Molecule::calc_bond_angle(&self.geom, j, k, l).sin();
+
+        let mut dihedral_angle: f64 = numerator / denom;
+
+        if dihedral_angle < -1.0 {
+            dihedral_angle = -1.0f64.acos();
+        } else if dihedral_angle > 1.0 {
+            dihedral_angle = 1.0f64.acos();
+        } else {
+            dihedral_angle = dihedral_angle.acos();
+        }
+
+        return dihedral_angle.to_degrees();
+    }
+
     // fn calc_oop_angle(geom: &Array2<f64>, idx1: usize, idx2: usize, idx3: usize, idx4: usize) -> f64 {
     //     let mut oop_angle: f64 = 0.0;
     //     let bond_length_buff_ij: f64 = Molecule::calc_bond_length(&geom, idx1, idx2);
@@ -154,7 +227,9 @@ fn main() {
 
     //* Step 1: Read file into buffer
     println!("Starting the OOP way:");
+    println!("test?!?");
     let mol: Molecule = Molecule::new("inp/geom.xyz", 0);
+    // let mol: Molecule = Molecule::new("QM_Programm/inp/geom.xyz", 0);
 
     println!("Z values of atoms:\n{:?}\n", mol.Z_vals);
     println!("Geometry of molecule:\n{:?}\n", mol.geom);
@@ -190,7 +265,7 @@ fn main() {
     }
 
     //* Step 4: OOP angles
-    println!("\n OOP angles (in degrees):\n");
+
     for i in 0..mol.no_atoms {
         for j in 0..mol.no_atoms {
             for k in 0..mol.no_atoms {
@@ -203,35 +278,38 @@ fn main() {
                         && i != l
                         && j != k
                         && k != l
-                        && j != l 
+                        && j != l
                         && bond_dist_jk < 4.0
                         && bond_dist_kl < 4.0
                         && bond_dist_ik < 4.0
                     {
-                        let mut unit_kj: Vec<f64> = vec![0.0; 3];
-                        let mut unit_kl: Vec<f64> = vec![0.0; 3];
-                        let mut unit_ki: Vec<f64> = vec![0.0; 3];
-
-                        for cart_coord in 0..3 {
-                            unit_kj[cart_coord] = (mol.geom[(k, cart_coord)]
-                                - mol.geom[(j, cart_coord)])
-                                / bond_dist_jk;
-                            unit_kl[cart_coord] = (mol.geom[(k, cart_coord)]
-                                - mol.geom[(l, cart_coord)])
-                                / bond_dist_kl;
-                            unit_ki[cart_coord] = (mol.geom[(k, cart_coord)]
-                                - mol.geom[(i, cart_coord)])
-                                / bond_dist_ik;
-                        }
-
-                        let cross_prod: Vec<f64> = Molecule::calc_vec_cros_prod(&unit_kj, &unit_kl);
-
-                        let mut oop_angle: f64 = Molecule::calc_scalar_prod(&cross_prod, &unit_ki);
-                        oop_angle = (oop_angle
-                            / Molecule::calc_bond_angle(&mol.geom, j, k, l).sin())
-                        .asin() * (180.0 / std::f64::consts::PI);
+                        let oop_angle: f64 = Molecule::calc_oop_angle(&mol, i, j, k, l);
 
                         println!("OOP angle for {}-{}-{}-{} is: {:.5}", i, j, k, l, oop_angle);
+                    }
+                }
+            }
+        }
+    }
+
+    // * Step 5: Torsion / dihedral angles
+
+    for i in 0..mol.no_atoms {
+        for j in 0..mol.no_atoms {
+            for k in 0..mol.no_atoms {
+                for l in 0..mol.no_atoms {
+                    let bond_dist_ij: f64 = Molecule::calc_bond_length(&mol.geom, i, j);
+                    let bond_dist_jk: f64 = Molecule::calc_bond_length(&mol.geom, j, k);
+                    let bond_dist_kl: f64 = Molecule::calc_bond_length(&mol.geom, k, l);
+                        && bond_dist_ij < 4.0
+                        && bond_dist_jk < 4.0
+                        && bond_dist_kl < 4.0
+                    {
+                        let dihedral_angle: f64 = Molecule::calc_dihedral_angle(&mol, i, j, k, l);
+                        println!(
+                            "Dihedral angle for {}-{}-{}-{} is: {:.5}",
+                            i, j, k, l, dihedral_angle
+                        );
                     }
                 }
             }
--- src/main_old.rs
+++ src/main_old.rs
@@ -95,65 +95,120 @@ fn main() {
     // println!("{:?}", bond_angles);
 
     //* Step 4: OOP angles
-
-    let mut oop_angles: Array4<f64> = Array4::zeros((no_atoms, no_atoms, no_atoms, no_atoms));
-    let mut unit_vec_kj: Vec<f64> = vec![0.0; 3];
-    let mut unit_vec_kl: Vec<f64> = vec![0.0; 3];
-    let mut unit_vec_ki: Vec<f64> = vec![0.0; 3];
-
-    for i in 0..no_atoms {
-        for j in 0..i {
-            for k in 0..j {
-                for l in 0..k {
-                    if i != j || i != k || i != l || j != k || j != l || k != l {
-                        let mut oop_angle: f64 = 0.0;
-                        let bond_length_buff_kj: f64 = calc_bond_length(&geom, i, j);
-                        let bond_length_buff_kl: f64 = calc_bond_length(&geom, j, k);
-                        let bond_length_buff_ki: f64 = calc_bond_length(&geom, k, i);
-
-                        //* Unit vec calcs */
-                        for cart_coord in 0..3 {
-                            unit_vec_kj[cart_coord] = (geom[(k, cart_coord)]
-                                - geom[(j, cart_coord)])
-                                / bond_length_buff_kj;
-                            unit_vec_kl[cart_coord] = (geom[(k, cart_coord)]
-                                - geom[(l, cart_coord)])
-                                / bond_length_buff_kl;
-                            unit_vec_ki[cart_coord] = (geom[(k, cart_coord)]
-                                - geom[(i, cart_coord)])
-                                / bond_length_buff_ki;
-                            }
-                        // println!("{:?}", unit_vec_kj);
-                        // println!("{:?}", unit_vec_kl);
-                        // println!("{:?}", unit_vec_ki);
-
-                        //* Cross product
-                        let cross_prod: Vec<f64> = calc_vec_cros_prod(&unit_vec_kj, &unit_vec_kl);
-                        // println!("{:?}", cross_prod);
-
-                        //* angle for oop
-                        let phi_jkl: f64 = calc_bond_angle(&geom, j, k, l);
+    println!("\n OOP angles (in degrees):\n");
+    for i in 0..mol.no_atoms {
+        for j in 0..mol.no_atoms {
+            for k in 0..mol.no_atoms {
+                for l in 0..mol.no_atoms {
+                    let bond_dist_jk: f64 = Molecule::calc_bond_length(&mol.geom, j, k);
+                    let bond_dist_kl: f64 = Molecule::calc_bond_length(&mol.geom, k, l);
+                    let bond_dist_ik: f64 = Molecule::calc_bond_length(&mol.geom, i, k);
+                    if i != j
+                        && i != k
+                        && i != l
+                        && j != k
+                        && k != l
+                        && j != l
+                        && bond_dist_jk < 4.0
+                        && bond_dist_kl < 4.0
+                        && bond_dist_ik < 4.0
+                    {
+                        let mut unit_kj: Vec<f64> = vec![0.0; 3];
+                        let mut unit_kl: Vec<f64> = vec![0.0; 3];
+                        let mut unit_ki: Vec<f64> = vec![0.0; 3];
 
                         for cart_coord in 0..3 {
-                            oop_angle +=
-                                cross_prod[cart_coord] * unit_vec_ki[cart_coord] / phi_jkl.sin();
+                            unit_kj[cart_coord] = (mol.geom[(k, cart_coord)]
+                                - mol.geom[(j, cart_coord)])
+                                / bond_dist_jk;
+                            unit_kl[cart_coord] = (mol.geom[(k, cart_coord)]
+                                - mol.geom[(l, cart_coord)])
+                                / bond_dist_kl;
+                            unit_ki[cart_coord] = (mol.geom[(k, cart_coord)]
+                                - mol.geom[(i, cart_coord)])
+                                / bond_dist_ik;
                         }
 
+                        let cross_prod: Vec<f64> = Molecule::calc_vec_cros_prod(&unit_kj, &unit_kl);
+
+                        let mut oop_angle: f64 =
+                            (Molecule::calc_scalar_prod(&cross_prod, &unit_ki))
+                                / Molecule::calc_bond_angle(&mol.geom, j, k, l).sin();
                         if oop_angle < -1.0 {
-                            oop_angle = -1.0_f64.asin();
+                            oop_angle = -1.0f64.asin();
+                        } else if oop_angle > 1.0 {
+                            oop_angle = 1.0f64.asin();
+                        } else {
+                            oop_angle = oop_angle.asin();
                         }
-                        else if oop_angle > 1.0 {
-                            oop_angle = 1.0_f64.asin();
-                        }
-                        else {
-                            oop_angle = oop_angle.asin() * (180.0 / std::f64::consts::PI);
-                        }
-                        // if oop_angle 
-                        oop_angles[(i, j, k, l)] = oop_angle;
+                        oop_angle = oop_angle.to_degrees();
+
+                        println!("OOP angle for {}-{}-{}-{} is: {:.5}", i, j, k, l, oop_angle);
                     }
                 }
             }
         }
     }
+
+    //! Not working… 
+    // let mut oop_angles: Array4<f64> = Array4::zeros((no_atoms, no_atoms, no_atoms, no_atoms));
+    // let mut unit_vec_kj: Vec<f64> = vec![0.0; 3];
+    // let mut unit_vec_kl: Vec<f64> = vec![0.0; 3];
+    // let mut unit_vec_ki: Vec<f64> = vec![0.0; 3];
+
+    // for i in 0..no_atoms {
+    //     for j in 0..i {
+    //         for k in 0..j {
+    //             for l in 0..k {
+    //                 if i != j || i != k || i != l || j != k || j != l || k != l {
+    //                     let mut oop_angle: f64 = 0.0;
+    //                     let bond_length_buff_kj: f64 = calc_bond_length(&geom, i, j);
+    //                     let bond_length_buff_kl: f64 = calc_bond_length(&geom, j, k);
+    //                     let bond_length_buff_ki: f64 = calc_bond_length(&geom, k, i);
+
+    //                     //* Unit vec calcs */
+    //                     for cart_coord in 0..3 {
+    //                         unit_vec_kj[cart_coord] = (geom[(k, cart_coord)]
+    //                             - geom[(j, cart_coord)])
+    //                             / bond_length_buff_kj;
+    //                         unit_vec_kl[cart_coord] = (geom[(k, cart_coord)]
+    //                             - geom[(l, cart_coord)])
+    //                             / bond_length_buff_kl;
+    //                         unit_vec_ki[cart_coord] = (geom[(k, cart_coord)]
+    //                             - geom[(i, cart_coord)])
+    //                             / bond_length_buff_ki;
+    //                         }
+    //                     // println!("{:?}", unit_vec_kj);
+    //                     // println!("{:?}", unit_vec_kl);
+    //                     // println!("{:?}", unit_vec_ki);
+
+    //                     //* Cross product
+    //                     let cross_prod: Vec<f64> = calc_vec_cros_prod(&unit_vec_kj, &unit_vec_kl);
+    //                     // println!("{:?}", cross_prod);
+
+    //                     //* angle for oop
+    //                     let phi_jkl: f64 = calc_bond_angle(&geom, j, k, l);
+
+    //                     for cart_coord in 0..3 {
+    //                         oop_angle +=
+    //                             cross_prod[cart_coord] * unit_vec_ki[cart_coord] / phi_jkl.sin();
+    //                     }
+
+    //                     if oop_angle < -1.0 {
+    //                         oop_angle = -1.0_f64.asin();
+    //                     }
+    //                     else if oop_angle > 1.0 {
+    //                         oop_angle = 1.0_f64.asin();
+    //                     }
+    //                     else {
+    //                         oop_angle = oop_angle.asin() * (180.0 / std::f64::consts::PI);
+    //                     }
+    //                     // if oop_angle 
+    //                     oop_angles[(i, j, k, l)] = oop_angle;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
     // println!("{:?}", oop_angles);
 }
